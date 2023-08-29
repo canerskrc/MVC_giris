7def8934910245c13a895619ed2b900183b34a4e
canersekerci <62672535+canerskrc@users.noreply.github.com>
2023-08-29 15:05:00 +0300

    Proje dosyası ekle.

---
+++ MVC_giris/Controllers/UrunlerControllers.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_giris.Controllers
+{
+    public class UrunlerControllers : Controller
+    {
+        public string Index()
+        {
+            return "Urunler sayfasına geçtiniz";
+        }
+    }
+}
